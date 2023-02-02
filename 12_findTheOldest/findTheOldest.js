9c4035d6f5950277b2b77ba626423470b7eda0a0
Iván Bobarini <inbobarini@gmail.com>
2023-02-02 18:19:00 -0300

    solve until exercice 12, included

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
-const findTheOldest = function() {
-
-};
+function findTheOldest(people) {
+    let lifeSpans =[];  
+    lifeSpans = people.map(e=>(e.yearOfDeath||2023)-e.yearOfBirth);
+    const max = Math.max(...lifeSpans)
+    const index = lifeSpans.indexOf(max)
+    return people[index]
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
